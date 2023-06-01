e27b6a7437b3ec7e8fa1362eec124d6634c26d83
PClark <133046368+pclark101@users.noreply.github.com>
2023-06-01 12:42:55 -0400

    Added comments

--- shodan_api.py
+++ shodan_api.py
@@ -1,18 +1,20 @@
-# REST API Call test using Shodan 
-
+# REST API test using Shodan 
 from shodan import Shodan
 
+#Creating writeable text file for output
 info_shodan = open('host.txt', 'w')
 
+#Hard-coded API key 
 api = Shodan('API-key')
 
 # Lookup an IP
 ipinfo = api.host('8.8.8.8')
 #print(ipinfo)
 
+#Writes content of Google DNS to text file 
 info_shodan.write(str(ipinfo))
 
-
+#Closes text file 
 info_shodan.close()
 
 
